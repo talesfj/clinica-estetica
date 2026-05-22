b9968fd96e9ba308d22c839b984c6e048fa411cc
Tales Santos <135122458+talesfj@users.noreply.github.com>
2026-05-22 20:54:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+
+});
